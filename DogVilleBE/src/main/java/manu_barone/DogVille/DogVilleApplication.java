caf3ffe70cc8b8cbbb0aca82a012511784a73009
Manuel Barone <baronemanuel05@gmail.com>
2024-11-18 15:24:10 +0100

    be added

---
+++ DogVilleBE/src/main/java/manu_barone/DogVille/DogVilleApplication.java
@@ -0,0 +1,13 @@
+package manu_barone.DogVille;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogVilleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogVilleApplication.class, args);
+	}
+
+}
