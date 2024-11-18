6835b1c58e70b7b8ed71deeeffa3131654fc23b0
Manuel Barone <baronemanuel05@gmail.com>
2024-11-18 14:07:43 +0100

    Init

---
+++ dogvilleFE/dogville/src/main/java/manu_barone/dogville/DogvilleApplication.java
@@ -0,0 +1,13 @@
+package manu_barone.dogville;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogvilleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogvilleApplication.class, args);
+	}
+
+}
---
+++ dogvilleFE/dogville/src/test/java/manu_barone/dogville/DogvilleApplicationTests.java
@@ -0,0 +1,13 @@
+package manu_barone.dogville;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogvilleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
